7975f0fee768fc61887d0264ebb2732e3a949db5
SungHo Ahn <jugiaro95@gmail.com>
2022-03-16 22:35:12 +0900

    Add nivo components

--- pactory/src/App.js
+++ pactory/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main'
+import { BrowserRouter as Router } from 'react-router-dom';
+import MyResponsiveLine  from './components/MyResponsiveLine/MyResponsiveLine'
+import { dataA } from './components/MyResponsiveLine/data';
+// import { ResponsiveLine } from '@nivo/line'
 
 function App() {
+  const dataB = [
+    {
+      "id": "japan",
+      "color": "hsl(349, 70%, 50%)",
+      "data": [
+        {
+          "x": 0,
+          "y": 31
+        },
+        {
+          "x": 118,
+          "y": 22
+        },
+        {
+          "x": 119,
+          "y": 239
+        },
+        {
+          "x": 120,
+          "y": 215
+        }
+      ]
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+      <Navbar/>
+      </Router>
+      <Main/>
+      <div>
+      <MyResponsiveLine data={dataB}/>
+      </div>
+      test
     </div>
   );
 }
